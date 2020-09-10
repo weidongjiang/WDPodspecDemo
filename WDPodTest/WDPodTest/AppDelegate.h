77eaab32605ec9e954c1d9876f4cf7d43a8c812c
weidong <jiangweidong@hetao101.com>
2020-09-10 17:04:54 +0800

    删除

---
+++ WDPodTest/WDPodTest/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  WDPodTest
+//
+//  Created by 伟东 on 2020/9/10.
+//  Copyright © 2020 伟东. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ WDPodTest/WDPodTestTests/WDPodTestTests.m
@@ -0,0 +1,37 @@
+//
+//  WDPodTestTests.m
+//  WDPodTestTests
+//
+//  Created by 伟东 on 2020/9/10.
+//  Copyright © 2020 伟东. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface WDPodTestTests : XCTestCase
+
+@end
+
+@implementation WDPodTestTests
+
+- (void)setUp {
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
